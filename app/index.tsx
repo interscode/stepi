0eae0dfab49dea5d050a946e13563faecbace846
mbalderasuaq <mbalderas08@alumnos.uaq.mx>
2025-03-28 17:08:21 -0600

    feat: add splash screen and tabler icons

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,57 @@
+import { useCallback, useEffect, useState } from "react";
 import { Text, View } from "react-native";
+import Entypo from "@expo/vector-icons/Entypo";
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
+import {
+  Inter_100Thin,
+  Inter_200ExtraLight,
+  Inter_300Light,
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+  Inter_900Black,
+  useFonts,
+} from "@expo-google-fonts/inter";
+import { IconBus } from "@tabler/icons-react-native";
+
+SplashScreen.preventAutoHideAsync();
+
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+  const [loaded, error] = useFonts({
+    Inter_100Thin,
+    Inter_200ExtraLight,
+    Inter_300Light,
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+    Inter_900Black,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
-    <View>
-      <Text>Hola Mundo</Text>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text style={{ fontFamily: "Inter_900Black" }}>Hola Mundo</Text>
+      <IconBus size={24} color="#000" />
     </View>
   );
 }
